ab84256e59ade618430592671a3151108668eb10
iklln6 <iklln6@me.com>
2016-02-18 07:54:16 -0600

    Update download_update_make.sh

--- download_update_make.sh
+++ download_update_make.sh
@@ -3,4 +3,5 @@
 wget https://github.com/iklln6/ocast_pi/archive/master.zip ~/Desktop/master.zip
 unzip ~/Desktop/master.zip -d ~/Desktop/foo
 cp ~/Desktop/foo/ocast_hello_world.c ~/Desktop/THUMB1GB/hello_pi/OCAST_heyworld/
+rm ~/Desktop/master.zip
 make
