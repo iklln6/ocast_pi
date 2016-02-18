dfecedbe1969b4d5e13a3796d53234f579868cb8
iklln6 <iklln6@me.com>
2016-02-18 08:14:40 -0600

    Update download_update_make.sh

--- download_update_make.sh
+++ download_update_make.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#wget https://github.com/iklln6/ocast_pi/archive/master.zip ~/Desktop/master.zip
+#unzip ~/Desktop/THUMB1GB/hello_pi/OCAST_heyworld/master.zip -d ~/Desktop/foo
+#cp ~/Desktop/foo/ocast_pi-master/ocast_hello_world.c ~/Desktop/THUMB1GB/hello_pi/OCAST_heyworld/
+#rm ~/Desktop/master.zip
+#make
 wget https://github.com/iklln6/ocast_pi/archive/master.zip ~/Desktop/master.zip
 unzip ~/Desktop/THUMB1GB/hello_pi/OCAST_heyworld/master.zip -d ~/Desktop/foo
-cp ~/Desktop/foo/ocast_pi-master/ocast_hello_world.c ~/Desktop/THUMB1GB/hello_pi/OCAST_heyworld/
-rm ~/Desktop/master.zip
+rm ~/Desktop/THUMB1GB/hello_pi/OCAST_heyworld/master.zip
+cp ~/Desktop/foo/ocast_pi-master/ocast_hello_world.c ~/Desktop/THUMB1GB/hello_pi/OCAST_heyworld
 make
