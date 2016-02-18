2fa22751bce3e3aa1c37a05197fc666e0c9a7448
iklln6 <iklln6@me.com>
2016-02-18 07:52:03 -0600

    Create download_update_make.sh

---
+++ download_update_make.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/iklln6/ocast_pi/archive/master.zip ~/Desktop/master.zip
+unzip ~/Desktop/master.zip -d ~/Desktop/foo
+cp ~/Desktop/foo/ocast_hello_world.c ~/Desktop/THUMB1GB/hello_pi/OCAST_heyworld/
+make
